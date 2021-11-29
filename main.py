f9d76fc64c334bbcbc9165bde5b3be98f4f43dc9
your_github_username <your_github_email>
2021-11-29 19:43:27 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import telebot
+from telebot import types
+
+token = "2131577582:AAG0gPUxFduEDlRQ51uJzuSCDMK6G0MVE30"
+
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    keyboard = types.ReplyKeyboardMarkup()
+    keyboard.row("Хочу", "/help")
+    bot.send_message(message.chat.id, 'Привет! Хочешь узнать свежую информацию о МТУСИ?', reply_markup=keyboard)
+
+
+@bot.message_handler(commands=['help'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Я умею...')
+
+
+@bot.message_handler(content_types=['text'])
+def answer(message):
+    if message.text.lower() == "хочу":
+        bot.send_message(message.chat.id, 'Тогда тебе сюда – https://mtuci.ru/')
+
+
+bot.infinity_polling()
